9fd937bc45c4408a9155d90d0762d420bbfd0c1b
Amit Paudel <amitpaudel789@gmail.com>
2025-09-04 13:23:59 +0545

    mongo atlas connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workOutRoutes = require('./routes/workout');
 
 const app = express();
@@ -12,6 +13,13 @@ app.use((req, res, next) => {
 
 app.use('/api/workouts', workOutRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running at http://localhost:${process.env.PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running at http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
